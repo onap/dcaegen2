eab16750256c4b4f54c3e1ac12282f4abe5d1b0d
Lusheng Ji <lji@research.att.com>
2017-09-19 18:39:04 +0000

    Update doc, mvn script, pom
    Issue-Id: DCAEGEN2-60
    Change-Id: Iad513b12fd79a6404d0ae05f65bce31951af918a
    Signed-off-by: Lusheng Ji <lji@research.att.com>

--- docs/index.rst
+++ docs/index.rst
@@ -1,7 +1,35 @@
 .. This work is licensed under a Creative Commons Attribution 4.0 International License.
 
-TODO Add files to toctree and delete this header
+Data Collection Analytics and Events
 ------------------------------------------------
+
+Data Collection Analytics and Events (DCAE) is the data collection and analysis subsystem of ONAP.
+Its functions include collection of FCAPs data from the network entotiess (VNFs, PNFs, etc), normalization and transportation of  
+data, analysis of data, and generations of ONAP events which can be received by other ONAP components such as Policy for
+subsequent operations.
+
+DCAE consists of DCAE Platform components and DCAE Services components.
+
+- DCAE Platform
+    - Core Platform
+        - Cloudify Manager
+        - Consul 
+    - Extended Platform
+        - Docker Host for containerized platform components
+        - CDAP Analytics Platform for CDAP analytics applications
+        - Docker Host for containerized service components
+
+
+- DCAE Services
+    - Collectors
+        - Virtual Event Streaming (VES) collector, dockerized
+        - SNMP Trap collector, dockerized
+    - Analytics
+        - Holmes correlation analytics, dockerized
+        - Threshold Crosssing Analytics (TCA), CDAP analytics application
+
+
+
 .. toctree::
    :maxdepth: 1
 
--- mvn-phase-lib.sh
+++ mvn-phase-lib.sh
@@ -19,6 +19,58 @@
 # ECOMP is a trademark and service mark of AT&T Intellectual Property.
 
 
+FQDN="${MVN_PROJECT_GROUPID}.${MVN_PROJECT_ARTIFACTID}"
+if [ "$MVN_PROJECT_MODULEID" == "__" ]; then
+  MVN_PROJECT_MODULEID=""
+fi
+
+if [[ "$MVN_PROJECT_VERSION" == *SNAPSHOT ]]; then
+  echo "=> for SNAPSHOT artifact build"
+  MVN_DEPLOYMENT_TYPE='SNAPSHOT'
+  MVN_DOCKERREGISTRY="$MVN_DOCKERREGISTRY_SNAPSHOT"
+  MVN_DOCKERREGISTRY_SERVERID="$MVN_DOCKERREGISTRY_SNAPSHOT_SERVERID"
+else
+  echo "=> for STAGING/RELEASE artifact build"
+  MVN_DEPLOYMENT_TYPE='STAGING'
+  # below is not mistake.  we only use the snapshot registry
+  MVN_DOCKERREGISTRY="$MVN_DOCKERREGISTRY_SNAPSHOT"
+  MVN_DOCKERREGISTRY_SERVERID="$MVN_DOCKERREGISTRY_SNAPSHOT_SERVERID"
+fi
+echo "MVN_DEPLOYMENT_TYPE is             [$MVN_DEPLOYMENT_TYPE]"
+
+
+TIMESTAMP=$(date +%C%y%m%dT%H%M%S)
+
+# WROKSPACE points to the current dir when the script is called, may be the root or module
+if [ -z "$WORKSPACE" ]; then
+  WORKSPACE=$(pwd)
+fi
+
+
+TIMESTAMP=$(date +%C%y%m%dT%H%M%S)
+
+
+
+echo "MVN_PROJECT_MODULEID is            [$MVN_PROJECT_MODULEID]"
+echo "MVN_PHASE is                       [$MVN_PHASE]"
+echo "MVN_PROJECT_GROUPID is             [$MVN_PROJECT_GROUPID]"
+echo "MVN_PROJECT_ARTIFACTID is          [$MVN_PROJECT_ARTIFACTID]"
+echo "MVN_PROJECT_VERSION is             [$MVN_PROJECT_VERSION]"
+echo "MVN_NEXUSPROXY is                  [$MVN_NEXUSPROXY]"
+echo "MVN_RAWREPO_BASEURL_UPLOAD is      [$MVN_RAWREPO_BASEURL_UPLOAD]"
+echo "MVN_RAWREPO_BASEURL_DOWNLOAD is    [$MVN_RAWREPO_BASEURL_DOWNLOAD]"
+MVN_RAWREPO_HOST=$(echo "$MVN_RAWREPO_BASEURL_UPLOAD" | cut -f3 -d'/' |cut -f1 -d':')
+echo "MVN_RAWREPO_HOST is                [$MVN_RAWREPO_HOST]"
+echo "MVN_RAWREPO_SERVERID is            [$MVN_RAWREPO_SERVERID]"
+echo "MVN_DOCKERREGISTRY is              [$MVN_DOCKERREGISTRY]"
+echo "MVN_DOCKERREGISTRY_SERVERID is     [$MVN_DOCKERREGISTRY_SERVERID]"
+echo "MVN_PYPISERVER_BASEURL is          [$MVN_PYPISERVER_BASEURL]"
+echo "MVN_PYPISERVER_SERVERID is         [$MVN_PYPISERVER_SERVERID]"
+echo "FQDN is                            [$FQDN]"
+echo "MVN_PROJECT_MODULEID is            [$MVN_PROJECT_MODULEID]"
+echo "MVN_DEPLOYMENT_TYPE is             [$MVN_DEPLOYMENT_TYPE]"
+
+
 clean_templated_files() 
 {
   TEMPLATE_FILES=$(find . -name "*-template")
@@ -226,40 +278,36 @@ upload_files_of_extension()
 }
 
 
+upload_files_of_extension_rec()
+{
+  FILES=$(find . -name  "*.${1}")
+  for F in $FILES ; do
+    upload_raw_file "$F"
+  done
+}
+
+
 
 build_and_push_docker()
 {
   IMAGENAME="onap/${FQDN}.${MVN_PROJECT_MODULEID}"
   IMAGENAME=$(echo "$IMAGENAME" | sed -e 's/_*$//g' -e 's/\.*$//g')
+  IMAGENAME=$(echo "$IMAGENAME" | tr '[:upper:]' '[:lower:]')
 
   # use the major and minor version of the MVN artifact version as docker image version
   VERSION="${MVN_PROJECT_VERSION//[^0-9.]/}"
   VERSION2=$(echo "$VERSION" | cut -f1-2 -d'.')
 
   LFQI="${IMAGENAME}:${VERSION}-${TIMESTAMP}"
-  #BUILD_PATH="${WORKSPACE}"
-  # build a docker image
-  #docker build --rm -f "${WORKSPACE}"/Dockerfile -t "${LFQI}" "${BUILD_PATH}"
-  #BUILD_PATH="${WORKSPACE}"
   # build a docker image
   docker build --rm -f ./Dockerfile -t "${LFQI}" ./
 
-  REPO=""
-  if [ $MVN_DEPLOYMENT_TYPE == "SNAPSHOT" ]; then
-     REPO=$MVN_DOCKERREGISTRY_DAILY
-  elif [ $MVN_DEPLOYMENT_TYPE == "STAGING" ]; then
-     # there seems to be no staging docker registry?  set to use SNAPSHOT also
-     #REPO=$MVN_DOCKERREGISTRY_RELEASE
-     REPO=$MVN_DOCKERREGISTRY_DAILY
-  else
-     echo "Fail to determine DEPLOYMENT_TYPE"
-     REPO=$MVN_DOCKERREGISTRY_DAILY
-  fi
-  echo "DEPLOYMENT_TYPE is: $MVN_DEPLOYMENT_TYPE, repo is $REPO"
+  REPO="$MVN_DOCKERREGISTRY"
+  SERVERID="$MVN_DOCKERREGISTRY_SERVERID"
 
   if [ ! -z "$REPO" ]; then
-    USER=$(xpath -e "//servers/server[id='$REPO']/username/text()" "$SETTINGS_FILE")
-    PASS=$(xpath -e "//servers/server[id='$REPO']/password/text()" "$SETTINGS_FILE")
+    USER=$(xpath -e "//servers/server[id='$SERVERID']/username/text()" "$SETTINGS_FILE")
+    PASS=$(xpath -e "//servers/server[id='$SERVERID']/password/text()" "$SETTINGS_FILE")
     if [ -z "$USER" ]; then
       echo "Error: no user provided"
     fi
@@ -283,6 +331,61 @@ build_and_push_docker()
 
     OLDTAG="${LFQI}"
     PUSHTAGS="${REPO}/${IMAGENAME}:${VERSION2}-${TIMESTAMP} ${REPO}/${IMAGENAME}:${VERSION2} ${REPO}/${IMAGENAME}:${VERSION2}-latest"
+    for NEWTAG in ${PUSHTAGS}
+    do
+      echo "tagging ${OLDTAG} to ${NEWTAG}"
+      docker tag "${OLDTAG}" "${NEWTAG}"
+      echo "pushing ${NEWTAG}"
+      docker push "${NEWTAG}"
+      OLDTAG="${NEWTAG}"
+    done
+  fi
+
+}
+
+
+
+push_docker_image()
+{
+  if [ -z "$1" ]; then
+     return
+  fi
+  OLDTAGNAME="$1"
+  OLDREPO=$(echo $TAGNAME | cut -f1 -d '/')
+  IMAGENAME_W_VERSION=$(echo $TAGNAME | cut -f2- -d '/')
+
+  # build a docker image
+  docker pull "$OLDTAGNAME"
+
+  REPO="$MVN_DOCKERREGISTRY"
+  SERVERID="$MVN_DOCKERREGISTRY_SERVERID"
+
+  if [ ! -z "$REPO" ]; then
+    USER=$(xpath -e "//servers/server[id='$SERVERID']/username/text()" "$SETTINGS_FILE")
+    PASS=$(xpath -e "//servers/server[id='$SERVERID']/password/text()" "$SETTINGS_FILE")
+    if [ -z "$USER" ]; then
+      echo "Error: no user provided"
+    fi
+    if [ -z "$PASS" ]; then
+      echo "Error: no password provided"
+    fi
+    [ -z "$PASS" ] && PASS_PROVIDED="<empty>" || PASS_PROVIDED="<password>"
+    echo docker login "$REPO" -u "$USER" -p "$PASS_PROVIDED"
+    docker login "$REPO" -u "$USER" -p "$PASS"
+
+    if [ $MVN_DEPLOYMENT_TYPE == "SNAPSHOT" ]; then
+      REPO="$REPO/snapshots"
+    elif [ $MVN_DEPLOYMENT_TYPE == "STAGING" ]; then
+      # there seems to be no staging docker registry?  set to use SNAPSHOT also
+      #REPO=$MVN_DOCKERREGISTRY_RELEASE
+      REPO="$REPO"
+    else
+      echo "Fail to determine DEPLOYMENT_TYPE"
+      REPO="$REPO/unknown"
+    fi
+
+    OLDTAG="${OLDTAGNAME}"
+    PUSHTAGS="${REPO}/${IMAGENAME_W_VERSION}-${TIMESTAMP} ${REPO}/${IMAGENAME_W_VERSION} ${REPO}/${IMAGENAME_W_VERSION}-latest"
     for NEWTAG in ${PUSHTAGS}
     do
       echo "tagging ${OLDTAG} to ${NEWTAG}"
