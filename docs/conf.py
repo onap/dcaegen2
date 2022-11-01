14a4c9a40dfa6f15a9578cf11fcf169cb24857f6
Vijay Venkatesh Kumar <vv770d@att.com>
2022-11-01 12:02:45 -0400

    Doc config updates
    Change-Id: If02f4d94a9a95a54db926d4c6262df08b7582664
    Issue-ID: DCAEGEN2-3301
    (cherry picked from commit 958df7b5a982a8633135bcd8a42a5339cb5fc7b4)
    Signed-off-by: Vijay Venkatesh Kumar <vv770d@att.com>

--- docs/conf.py
+++ docs/conf.py
@@ -1,11 +1,31 @@
-from docs_conf.conf import *
-
-branch = 'latest'
+project = "onap"
+release = "kohn"
+version = "kohn"
+branch = 'kohn'
 master_doc = 'index'
 
 linkcheck_ignore = [
-    'http://localhost',
+  r'http://localhost:\d+/'
 ]
+
+extensions = [
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.graphviz',
+    'sphinxcontrib.blockdiag',
+    'sphinxcontrib.seqdiag',
+    'sphinxcontrib.swaggerdoc',
+    'sphinxcontrib.plantuml'
+]
+
+pygments_style = "sphinx"
+html_theme = "sphinx_rtd_theme"
+html_theme_options = {
+  "style_nav_header_background": "white",
+  "sticky_navigation": "False" }
+html_logo = "_static/logo_onap_2017.png"
+html_favicon = "_static/favicon.ico"
+html_static_path = ["_static"]
+html_show_sphinx = False
 
 intersphinx_mapping = {}
 
@@ -13,3 +33,6 @@ html_last_updated_fmt = '%d-%b-%y %H:%M'
 
 def setup(app):
     app.add_css_file("css/ribbon.css")
+
+
+
